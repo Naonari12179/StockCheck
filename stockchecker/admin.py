ac6c3cdeadb275bea6b1ed6b0385db63528bcd22
Tomonita <naonao12179@gmail.com>
2020-11-29 16:02:06 +0900

    My Django taniproject app, first commit

--- stockchecker/admin.py
+++ stockchecker/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 
 from django.contrib import admin
-from .models import   Categorys, お菓子A, お菓子B, お菓子C, 半生お菓子
+from .models import   Categorys, お菓子UZAWA, お菓子FUKUCHI, お菓子, 半生お菓子, チーズ, 洋日配, 飲料, お酒
 
 
 
 admin.site.register(Categorys)
 
-class お菓子AAdmin(admin.ModelAdmin):
+class お菓子UZAWAAdmin(admin.ModelAdmin):
     fieldsets = [('商品名', {'fields': ['商品名']}),
                  ('賞味', {'fields': ['dis_date'],'classes': ['collapse']}),
                  
@@ -16,9 +16,10 @@ class お菓子AAdmin(admin.ModelAdmin):
     list_filter = ['dis_date','category']
     list_editable = ['category']
     search_fields = ['商品名']
-admin.site.register(お菓子A, お菓子AAdmin)
+    ordering = ('dis_date',)
+admin.site.register(お菓子UZAWA, お菓子UZAWAAdmin)
 
-class お菓子BAdmin(admin.ModelAdmin):
+class お菓子FUKUCHIAdmin(admin.ModelAdmin):
     fieldsets = [('商品名', {'fields': ['商品名']}),
                  ('賞味', {'fields': ['dis_date'],'classes': ['collapse']}),
                  ]
@@ -26,9 +27,10 @@ class お菓子BAdmin(admin.ModelAdmin):
     list_filter = ['dis_date','category']
     search_fields = ['商品名']
     list_editable = ['category']
-admin.site.register(お菓子B, お菓子BAdmin)
+    ordering = ('dis_date',)
+admin.site.register(お菓子FUKUCHI, お菓子FUKUCHIAdmin)
 
-class お菓子CAdmin(admin.ModelAdmin):
+class お菓子Admin(admin.ModelAdmin):
     fieldsets = [('商品名', {'fields': ['商品名']}),
                  ('賞味', {'fields': ['dis_date'],'classes': ['collapse']}),
                  ]
@@ -36,7 +38,8 @@ class お菓子CAdmin(admin.ModelAdmin):
     list_filter = ['dis_date','category']
     search_fields = ['商品名']
     list_editable = ['category']
-admin.site.register(お菓子C, お菓子CAdmin)
+    ordering = ('dis_date',)
+admin.site.register(お菓子, お菓子Admin)
 
 class 半生お菓子Admin(admin.ModelAdmin):
     fieldsets = [('商品名', {'fields': ['商品名']}),
@@ -46,4 +49,50 @@ class 半生お菓子Admin(admin.ModelAdmin):
     list_filter = ['dis_date','category']
     search_fields = ['商品名']
     list_editable = ['category']
+    ordering = ('dis_date',)
 admin.site.register(半生お菓子, 半生お菓子Admin)
+
+class 洋日配Admin(admin.ModelAdmin):
+    fieldsets = [('商品名', {'fields': ['商品名']}),
+                 ('賞味', {'fields': ['dis_date'],'classes': ['collapse']}),
+                 ]
+    list_display = ('商品名','category','賞味期限','見切り1','見切り2', '見切り3')
+    list_filter = ['dis_date','category']
+    search_fields = ['商品名']
+    list_editable = ['category']
+    ordering = ('dis_date',)
+admin.site.register(洋日配, 洋日配Admin)
+
+class チーズAdmin(admin.ModelAdmin):
+    fieldsets = [('商品名', {'fields': ['商品名']}),
+                 ('賞味', {'fields': ['dis_date'],'classes': ['collapse']}),
+                 ]
+    list_display = ('商品名','category','賞味期限','見切り1','見切り2', '見切り3')
+    list_filter = ['dis_date','category']
+    search_fields = ['商品名']
+    list_editable = ['category']
+    ordering = ('dis_date',)
+admin.site.register(チーズ, チーズAdmin)
+
+
+class 飲料Admin(admin.ModelAdmin):
+    fieldsets = [('商品名', {'fields': ['商品名']}),
+                 ('賞味', {'fields': ['dis_date'],'classes': ['collapse']}),
+                 ]
+    list_display = ('商品名','category','賞味期限','見切り1','見切り2', '見切り3')
+    list_filter = ['dis_date','category']
+    search_fields = ['商品名']
+    list_editable = ['category']
+    ordering = ('dis_date',)
+admin.site.register(飲料, 飲料Admin)
+
+class お酒Admin(admin.ModelAdmin):
+    fieldsets = [('商品名', {'fields': ['商品名']}),
+                 ('賞味', {'fields': ['dis_date'],'classes': ['collapse']}),
+                 ]
+    list_display = ('商品名','category','賞味期限','見切り1','見切り2', '見切り3')
+    list_filter = ['dis_date','category']
+    search_fields = ['商品名']
+    list_editable = ['category']
+    ordering = ('dis_date',)
+admin.site.register(お酒, お酒Admin)
--- stockchecker/models.py
+++ stockchecker/models.py
@@ -2,6 +2,7 @@
 
 import datetime
 from datetime import  timedelta
+from dateutil.relativedelta import relativedelta
 from django.db import models
 from django.utils import timezone
 
@@ -14,7 +15,7 @@ class Categorys(models.Model):
         verbose_name = 'カテゴリー'
         verbose_name_plural = 'カテゴリー'
         
-class お菓子A(models.Model):
+class お菓子UZAWA(models.Model):
     商品名 = models.CharField(max_length=200)
     dis_date = models.DateTimeField('賞味')
     category = models.ForeignKey(Categorys, on_delete=models.PROTECT,default=1)
@@ -34,9 +35,9 @@ class お菓子A(models.Model):
         sale3 = self.dis_date - timedelta(days=7)
         return sale3.date()
     class Meta:
-        verbose_name = 'お菓子A'
-        verbose_name_plural = 'お菓子A'
-class お菓子B(models.Model):
+        verbose_name = 'お菓子UZAWA'
+        verbose_name_plural = 'お菓子UZAWA'
+class お菓子FUKUCHI(models.Model):
     商品名 = models.CharField(max_length=200)
     dis_date = models.DateTimeField('賞味')
     category = models.ForeignKey(Categorys, on_delete=models.PROTECT,default=1)
@@ -56,10 +57,10 @@ class お菓子B(models.Model):
         sale3 = self.dis_date - timedelta(days=7)
         return sale3.date()
     class Meta:
-        verbose_name = 'お菓子B'
-        verbose_name_plural = 'お菓子B'
+        verbose_name = 'お菓子FUKUCHI'
+        verbose_name_plural = 'お菓子FUKUCHI'
 
-class お菓子C(models.Model):
+class お菓子(models.Model):
     商品名 = models.CharField(max_length=200)
     dis_date = models.DateTimeField('賞味')
     category = models.ForeignKey(Categorys, on_delete=models.PROTECT,default=1)
@@ -79,8 +80,8 @@ class お菓子C(models.Model):
         sale3 = self.dis_date - timedelta(days=7)
         return sale3.date()
     class Meta:
-        verbose_name = 'お菓子C'
-        verbose_name_plural = 'お菓子C'
+        verbose_name = 'お菓子'
+        verbose_name_plural = 'お菓子'
 class 半生お菓子(models.Model):
     商品名 = models.CharField(max_length=200)
     dis_date = models.DateTimeField('賞味')
@@ -103,3 +104,92 @@ class 半生お菓子(models.Model):
     class Meta:
         verbose_name = '半生お菓子'
         verbose_name_plural = '半生お菓子'
+class 洋日配(models.Model):
+    商品名 = models.CharField(max_length=200)
+    dis_date = models.DateTimeField('賞味')
+    category = models.ForeignKey(Categorys, on_delete=models.PROTECT,default=1)
+    
+    def __str__(self):
+        return self.商品名
+    def 賞味期限(self):
+        best_by = self.dis_date
+        return best_by.date()
+    def 見切り1(self):
+        sale1 = self.dis_date - timedelta(days=5)
+        return sale1.date()
+    def 見切り2(self):
+        sale2 = self.dis_date - timedelta(days=2)
+        return sale2.date()
+    def 見切り3(self):
+        sale3 = self.dis_date - timedelta(days=1)
+        return sale3.date()
+    class Meta:
+        verbose_name = '洋日配'
+        verbose_name_plural = '洋日配'
+class チーズ(models.Model):
+    商品名 = models.CharField(max_length=200)
+    dis_date = models.DateTimeField('賞味')
+    category = models.ForeignKey(Categorys, on_delete=models.PROTECT,default=1)
+    
+    def __str__(self):
+        return self.商品名
+    def 賞味期限(self):
+        best_by = self.dis_date
+        return best_by.date()
+    def 見切り1(self):
+        sale1 = self.dis_date - timedelta(days=7)
+        return sale1.date()
+    def 見切り2(self):
+        sale2 = self.dis_date - timedelta(days=3)
+        return sale2.date()
+    def 見切り3(self):
+        sale3 = self.dis_date - timedelta(days=1)
+        return sale3.date()
+    class Meta:
+        verbose_name = 'チーズ'
+        verbose_name_plural = 'チーズ'
+class 飲料(models.Model):
+    商品名 = models.CharField(max_length=200)
+    dis_date = models.DateTimeField('賞味')
+    category = models.ForeignKey(Categorys, on_delete=models.PROTECT,default=1)
+    
+    def __str__(self):
+        return self.商品名
+    def 賞味期限(self):
+        best_by = self.dis_date
+        return best_by.date()
+    def 見切り1(self):
+        sale1 = self.dis_date - timedelta(days=14)
+        return sale1.date()
+    def 見切り2(self):
+        sale2 = self.dis_date - timedelta(days=7)
+        return sale2.date()
+    def 見切り3(self):
+        sale3 = self.dis_date - timedelta(days=3)
+        return sale3.date()
+    class Meta:
+        verbose_name = '飲料'
+        verbose_name_plural = '飲料'
+
+class お酒(models.Model):
+    商品名 = models.CharField(max_length=200)
+    dis_date = models.DateTimeField('賞味')
+    category = models.ForeignKey(Categorys, on_delete=models.PROTECT,default=1)
+    
+    def __str__(self):
+        return self.商品名
+    def 賞味期限(self):
+        best_by = self.dis_date
+        return best_by.date()
+    def 見切り1(self):
+        sale1 = self.dis_date - relativedelta(months=1)
+        return sale1.date()
+    def 見切り2(self):
+        sale2 = self.dis_date - timedelta(days=17)
+        return sale2.date()
+    def 見切り3(self):
+        sale3 = self.dis_date - timedelta(days=7)
+        return sale3.date()
+    class Meta:
+        verbose_name = 'お酒'
+        verbose_name_plural = 'お酒'
